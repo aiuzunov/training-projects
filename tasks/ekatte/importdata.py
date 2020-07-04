e40a443e5828b03152e172e3833bb06a97715b03
root <aleksandar.i.uzunov@gmail.com>
2020-07-04 21:07:34 +0300

    Can be executed multiple times + added statistics

--- tasks/ekatte/importdata.py
+++ tasks/ekatte/importdata.py
@@ -29,6 +29,9 @@ for i in range(1,broi_ekatte):
 	ekatte_result = ekatte_result + "('%s', '%s', '%s','%s'),"%(ekatte['ekatte'][i],ekatte['t_v_m'][i],ekatte['name'][i],ekatte['obstina'][i])
 ekatte_result = ekatte_result[:-1]
 
+oblasti_result += ' ON CONFLICT (oblast) DO UPDATE SET oblast = oblasti.oblast'
+obshtini_result += ' ON CONFLICT (obshtina) DO UPDATE SET obshtina = obshtini.obshtina'
+ekatte_result += ' ON CONFLICT (ekatte) DO UPDATE SET ekatte = ekatte.ekatte'
 
 connection = psycopg2.connect(
 	database = 'ekatte',
@@ -41,14 +44,27 @@ connection = psycopg2.connect(
 sql = connection.cursor()
 
 sql.execute(oblasti_result)
+obl_stat = sql.rowcount
 connection.commit()
+
+
 
 sql.execute(obshtini_result)
+obs_stat = sql.rowcount
 connection.commit()
 
+
+
 sql.execute(ekatte_result)
+ekt_stat = sql.rowcount
 connection.commit()
+
 
 sql.close()
 connection.close()
 
+print("Брой на импортираните области = {:d}".format(obl_stat))
+
+print("Брой на импортираните общини = {:d}".format(obs_stat))
+
+print("Брой на импортираните населени места = {:d}".format(ekt_stat))
