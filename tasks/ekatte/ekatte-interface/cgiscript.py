db875a69bd6ed8391eeb0cca1feec6c03cda2acb
Alexander Uzunov <aleksandar.i.uzunov@gmail.com>
2020-07-14 20:59:24 +0300

    Fix for the cgi script

--- tasks/ekatte/ekatte-interface/cgiscript.py
+++ tasks/ekatte/ekatte-interface/cgiscript.py
@@ -1,48 +1,46 @@
 #!/usr/bin/python3
 
-import cgi,cgitb, psycopg2
+import cgi, cgitb, psycopg2, html
 
-print ("Content-type:text/html; charset=utf-8\r\n\r\n")
-print ("<html>")
-print ("<head>")
+print("Content-type:text/html; charset=utf-8\r\n\r\n")
+print("<html>")
+print("<head>")
 
-print ("<style>")
+print("<style>")
 print("table, th, td {")
 print("border: 1px solid black;")
 print("}")
-print ("td {")
-print ("display: table-cell;")
-print ("text-align:left;")
-print ("}")
-print ("</style>")
- 
+print("td {")
+print("display: table-cell;")
+print("text-align:left;")
+print("}")
+print("</style>")
 
-print ("<title>Ekatte Task</title>")
-print ("</head>")
-print ("<body style='background-color:rgb(211, 159, 149)'>")
-print ("<h3> Търсене на информация за населено място </h3>")
-print ('<form name="userinputform" method="POST" action = "cgiscript.py"> <input name="name" type="text" placeholder = "Име на населено място" pattern="[а-яА-Я ]*" required /> </form>')
-
+print("<title>Ekatte Task</title>")
+print("</head>")
+print("<body style='background-color:rgb(211, 159, 149)'>")
+print("<h3> Търсене на информация за населено място </h3>")
+print(
+    '<form name="userinputform" method="POST" action = "cgiscript.py"> <input name="name" type="text" pattern="[а-яА-Я \']*" placeholder = "Име на населено място" required /> </form>')
 form = cgi.FieldStorage()
 input = form.getvalue("name")
+input = html.escape(input)
+print("Вие търсихте населено място по име с ключовата дума: {:s}".format(input))
 
 connection = psycopg2.connect(
-	database = 'ekatte',
-	user = 'ekatte',
-	password = 'randompassword1234',
-	host = '127.0.0.1',
-	port = '5432'
+    database='ekatte',
+    user='ekatte',
+    password='randompassword1234',
+    host='127.0.0.1',
+    port='5432'
 )
 
 sql = connection.cursor()
 
-sql.execute("SELECT selishta.ekatte, oblasti.name, obshtini.name, selishta.t_v_m, selishta.name FROM oblasti,obshtini,selishta WHERE LOWER(selishta.name) LIKE concat(LOWER(%s),%s) AND oblasti.oblast_id = obshtini.oblast_id AND obshtini.obshtina_id = selishta.obshtina_id ORDER BY selishta.ekatte;",(input,'%'))
-
+sql.execute(
+    "SELECT selishta.ekatte, oblasti.name, obshtini.name, selishta.t_v_m, selishta.name FROM oblasti,obshtini,selishta WHERE LOWER(selishta.name) LIKE concat(LOWER(%s),%s) AND oblasti.oblast_id = obshtini.oblast_id AND obshtini.obshtina_id = selishta.obshtina_id ORDER BY selishta.ekatte;",
+    (input, '%'))
 results = sql.fetchall()
-connection.commit()
-sql = connection.cursor()
-
-sql = connection.cursor()
 
 oblselect = 'SELECT count(*) from oblasti'
 sql.execute(oblselect)
@@ -56,26 +54,21 @@ ekselect = 'SELECT count(*) from selishta'
 sql.execute(ekselect)
 ekt_stat = sql.fetchone()
 
-
 sql.close()
 connection.close()
-
-
-
-
 
 print("<table> <tr> <th>ЕКАТТЕ</th> <th>Област</th> <th>Община</th> <th>Вид</th> <th>Име</th> </tr>")
 
 for row in results:
-	print("<tr> <td>",row[0],"</td> <td>",row[1],"</td> <td>",row[2],"</td> <td>",row[3],"</td> <td>",row[4],"</td> </tr>")
+    print("<tr> <td>", row[0], "</td> <td>", row[1], "</td> <td>", row[2], "</td> <td>", row[3], "</td> <td>", row[4],
+          "</td> </tr>")
 
 print("</tr></table>")
 print("<br><br>")
 
 print("<table> <tr> <th>Брой Области</th> <th>Брой Общини</th> <th>Брой Селища</th> </tr>")
-print("<tr> <td>",obl_stat[0],"</td> <td>",obs_stat[0],"</td> <td>",ekt_stat[0],"</td> </tr>")
+print("<tr> <td>", obl_stat[0], "</td> <td>", obs_stat[0], "</td> <td>", ekt_stat[0], "</td> </tr>")
 print("</tr></table>")
 
-
-print ("</body>")
-print ("</html>")
+print("</body>")
+print("</html>")
