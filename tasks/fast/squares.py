8e4b3d5d0f8e34ec7d95210b85ae33f6b6cb702e
Alexander Uzunov <aleksandar.i.uzunov@gmail.com>
2020-08-07 23:25:14 +0300

    Addint the a quick task for drawing a circle and rework for the squares fast task

--- tasks/fast/squares.py
+++ tasks/fast/squares.py
@@ -1,75 +1,78 @@
-def checkBox(x1, y1, x2, y2):
-    count = 0
-    for i in range(x1, x2):
-        for j in range(y1, y2):
-            if square[i][j] == '0':
-              valid = 1
-              for k in range(n*(i//n), n + n*(i//n)):
-                  for l in range(n*(j//n), n + n*(j//n)):
-                    if square[k][l] == s:
-                      valid = 0
-              for k in range(size):
-                if square[i][k] == s: 
-                  valid = 0
-              for k in range(size):
-                if square[k][j] == s:
-                  valid = 0
-              if valid:
-                count += 1
-    return count
-
 n = int(input())
 
 while n < 2 or n > 3:
-    print("Select a number between 2 and 3")
+    print('Невалиден вход')
     n = int(input())
 
-size = n*n
+
+size = n**2
+
+check = []
 square = []
 symbols = []
 
 for i in range(size):
     line = input().split()
+    arr = []
+    for x in range(len(line)):
+        arr.append(line[x])
     square.append(line)
+    check.append(arr)
 
 for i in range(size):
     for j in range(size):
-      if square[i][j] not in symbols and square[i][j] != '0':
-        symbols.append(square[i][j])
+      if square[i][j] not in symbols:
+          if square[i][j] != '0':
+            symbols.append(square[i][j])
 
-solved = 0
-while not solved:
-  solved = 1
-  for i in range(size):
-      for j in range(size):
-        if square[i][j] == '0':
-          solved = 0
+if len(symbols) < size:
+    raise Exception("Броят на символите не може да бъде по - малък от размера на матрицата")
 
-  for s in symbols:
-    for i in range(size):
-        for j in range(size):
-            if square[i][j] == '0':
-              valid = 1
+def isValid(x, y, s):
+    correct = True
 
-              for k in range(n*(i//n), n + n*(i//n)): 
-                  for l in range(n*(j//n), n + n*(j//n)):
-                    if square[k][l] == s:
-                      valid = 0
+    for k in range(size):
+        if square[x][k] == s or square[k][y]==s: 
+          correct = False
 
-              for k in range(size):
-                if square[i][k] == s: 
-                  valid = 0
+    for k in range(n*(x//n), n + n*(x//n)): 
+      for l in range(n*(y//n), n + n*(y//n)):
+        if square[k][l] == s:
+          correct = False
 
-              for k in range(size):
-                if square[k][j] == s: 
-                  valid = 0
+    return correct
 
-              if valid and checkBox(n*(i//n), n*(j//n), n + n*(i//n), n + n*(j//n)) == 1:
-                square[i][j] = s
+def CheckAndReplace(x, y):
+    if x == size-1:
+        nextPoint = (0, y+1)
+    else:
+        nextPoint = (x+1, y)
+    if(square[x][y] == '0'):
+        for s in symbols:
+            if isValid(x, y, s):
+                square[x][y] = s
+                if isSolved():
+                    print('Резултат:')
+                    for i in range(size):
+                        for j in range(size):
+                            if square[i][j] == check[i][j]:
+                                print(square[i][j], end=" ")
+                            else:
+                                print(square[i][j], end=" ")
+                        print()
+                else:
+                    CheckAndReplace(nextPoint[0], nextPoint[1])
+        square[x][y] = '0'
+    else:
+        if x != size-1 or y != size-1:
+            CheckAndReplace(nextPoint[0], nextPoint[1])
 
+def isSolved():
+    solved = True
+    for x in range(size):
+        for y in range(size):
+            if square[x][y] == '0':
+                solved = False
+    return solved
 
-print("Result:")
-for i in range(size):
-    for j in range(size):
-        print(square[i][j],end=" ")
-    print()
+CheckAndReplace(0, 0)
