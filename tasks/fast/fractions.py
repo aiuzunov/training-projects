dd0b61301e37d7eda0e5470d0dbda7083efc45c8
aiuzunov <aleksandar.i.uzunov@gmail.com>
2020-09-01 16:10:30 +0300

    Yet another fix for the fractions fast task

--- tasks/fast/fractions.py
+++ tasks/fast/fractions.py
@@ -3,9 +3,11 @@ inputArray = input().split(" ")
 z = int(inputArray[0])
 m = int(inputArray[1])
 n = int(inputArray[2])
+lower_p = 0
+lower_q = 0
+upper_p = 0
+upper_q = 0
 
-chislitel = m+1
-znamenatel = 1
 
 def findDivisor(a,b):
     GCD=0
@@ -21,15 +23,44 @@ def findDivisor(a,b):
         big = GCD
     return GCD
 
-while chislitel<z:
-    while znamenatel<=z:
-        if chislitel<znamenatel and findDivisor(chislitel,znamenatel)==1:
-                print(str(chislitel)+" "+str(znamenatel))
-                exit()
-        else:
-            znamenatel+=1
-    chislitel+=1
-    znamenatel=1
 
+def findLower():
+    global lower_p
+    global lower_q
+    chislitel = m
+    znamenatel = n-1
+    while chislitel<z:
+        while znamenatel>chislitel:
+            if chislitel<znamenatel and findDivisor(chislitel,znamenatel)==1:
+                lower_p = chislitel
+                lower_q = znamenatel
+                return chislitel/znamenatel
+            else:
+                znamenatel-=1
+        chislitel-=1
+        znamenatel=z
 
-print("There is no such element")
+def findUpper():
+    global upper_p
+    global upper_q
+    chislitel = m+1
+    znamenatel = 1
+    while chislitel<z:
+        while znamenatel<=z:
+            if chislitel<znamenatel and findDivisor(chislitel,znamenatel)==1:
+                upper_p = chislitel
+                upper_q = znamenatel
+                return chislitel/znamenatel
+            else:
+                znamenatel+=1
+        chislitel+=1
+        znamenatel=1
+
+findLower()
+findUpper()
+
+if lower_p/lower_q<upper_p/upper_q:
+    if lower_p/lower_q>m/n:
+        print(str(lower_p)+" "+str(lower_q))
+    else:
+        print(str(upper_p)+" "+str(upper_q))
