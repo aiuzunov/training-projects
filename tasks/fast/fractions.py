e313d58994a9b21e0a26614d474490f0667c26c0
aiuzunov <aleksandar.i.uzunov@gmail.com>
2020-09-01 12:26:11 +0300

    Fixes for the fractions.py task

--- tasks/fast/fractions.py
+++ tasks/fast/fractions.py
@@ -3,9 +3,9 @@ inputArray = input().split(" ")
 z = int(inputArray[0])
 m = int(inputArray[1])
 n = int(inputArray[2])
-drobi = []
-chislitel = 1
-znamenatel = 1
+
+chislitel = m
+znamenatel = n+1
 
 def findDivisor(a,b):
     GCD=0
@@ -24,12 +24,9 @@ def findDivisor(a,b):
 while chislitel<z:
     while znamenatel<=z:
         if chislitel<znamenatel and findDivisor(chislitel,znamenatel)==1:
-                drobi.append(str(chislitel)+' '+str(znamenatel))
-                znamenatel+=1
+                print(str(chislitel)+" "+str(znamenatel))
+                exit()
         else:
             znamenatel+=1
     chislitel+=1
     znamenatel=1
-
-
-print(drobi[drobi.index(str(m)+" "+str(n))+1])
